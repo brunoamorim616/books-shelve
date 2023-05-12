4eaf1b017d54466993fad06ba50b683a1c5476c2
bruno <amorimbq@gmail.com>
2023-05-11 21:32:57 -0300

    refactor: updated nytimes api key config

--- packages/app/api/index.ts
+++ packages/app/api/index.ts
@@ -1,8 +1,9 @@
 import { create } from 'apisauce'
 import { NYTIMES_API_URL } from 'app/config'
+import secrets from '../../../secrets.json'
 
 export const nytimesApi = create({
   baseURL: NYTIMES_API_URL,
   headers: { 'Content-Type': 'application/json' },
-  params: { 'api-key': '' },
+  params: { 'api-key': secrets.nytimes_api_key },
 })
