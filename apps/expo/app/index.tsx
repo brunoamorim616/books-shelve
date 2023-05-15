b2279b4affdd0a282d169e95b050ff07f82ee474
bruno <amorimbq@gmail.com>
2023-05-15 13:05:48 -0300

    wip

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,16 +1,10 @@
 import { HomeScreen } from 'app/features/home/screen'
 import { Stack } from 'expo-router'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function Screen() {
-  const { top } = useSafeAreaInsets()
   return (
     <>
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-      />
+      <Stack.Screen options={{ headerShown: false }} />
       <HomeScreen />
     </>
   )
